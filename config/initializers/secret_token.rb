7cec1001c7481986a96e98c7eb6712d81406065d
Marco Tessari <marco.tessari@gmail.com>
2010-11-21 19:19:42 +0100

    Upgrade to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockstails::Application.config.secret_token = '8585552ff7bf7087e533b3ba416babc324b6b8b2848303ae06c74af5d3f071339a8592977bf8d2c0a7bd884236bca32b15d9964a0aeec89b0507efd300be96c5'
