795709fef10ee348d46c167670f4d82df73eea49
Marco Tessari <marco.tessari@gmail.com>
2010-11-22 22:41:40 +0100

    Try to fix mongodb on heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,7 +1,6 @@
 if ENV['MONGOHQ_URL']
-	uri = URI.parse(ENV['MONGOHQ_URL'])
-	MongoMapper.connection = Mongo::Connection.from_uri(uri)
-	MongoMapper.database = MongoMapper.connection.db(uri.path.gsub(/^\//, ''))
+	MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+	MongoMapper.connect(RAILS_ENV)
 else
 	MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 	MongoMapper.database = "rockstails-#{Rails.env}"
