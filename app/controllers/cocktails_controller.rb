a245dff26e553517c4d577d977e16b0dbcfb561e
Sylvain Veyrié <sylvain.veyrie@gmail.com>
2010-11-23 23:57:26 +0100

    Use .first([ id ]) instead of .find(id) as it is more suitable for MongoDB design

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,7 +13,7 @@ class CocktailsController < ApplicationController
   # GET /cocktails/1
   # GET /cocktails/1.xml
   def show
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.first([ params[:id] ])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -34,7 +34,7 @@ class CocktailsController < ApplicationController
 
   # GET /cocktails/1/edit
   def edit
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.first([ params[:id] ])
   end
 
   # POST /cocktails
@@ -56,7 +56,7 @@ class CocktailsController < ApplicationController
   # PUT /cocktails/1
   # PUT /cocktails/1.xml
   def update
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.first([ params[:id] ])
 
     respond_to do |format|
       if @cocktail.update_attributes(params[:cocktail])
@@ -71,15 +71,15 @@ class CocktailsController < ApplicationController
 
   # GET /cocktail/1/select_ingredient
   def select_ingredient
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.first([ params[:id] ])
     @ingredients = Ingredient.all
   end
 
   # PUT /cocktail/1/add_ingredient
   # PUT /cocktail/1/add_ingredient.xml
   def add_ingredient
-    @cocktail = Cocktail.find(params[:id])
-    @ingredient = Ingredient.find(params["ingredient"]["id"])
+    @cocktail = Cocktail.first([ params[:id] ])
+    @ingredient = Ingredient.first([ params["ingredient"]["id"] ])
 
     respond_to do |format|
       if @cocktail.ingredients << @ingredient
@@ -96,7 +96,7 @@ class CocktailsController < ApplicationController
   # DELETE /cocktails/1
   # DELETE /cocktails/1.xml
   def destroy
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.first([ params[:id] ])
     @cocktail.destroy
 
     respond_to do |format|
