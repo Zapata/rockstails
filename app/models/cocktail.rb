c642babe13eb11329ba4412e8d2fd53f7b5de6e6
Sylvain Veyrié <sylvain.veyrie@gmail.com>
2010-11-25 11:30:12 +0100

    Tests: add ingredient to cocktail from dirty_cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,8 +4,21 @@ class Cocktail
   key :name,  String, :require => true
   many :ingredients
 
-  def copy_from cocktail
-    @name = cocktail.name
+  def copy_from dirty_cocktail
+    # properties
+    @name = dirty_cocktail.name
+
+    # ingredients
+    dirty_cocktail.ingredients.each do |dirty_ingredient|
+      # dirty cocktail ingredients : [ amount, doze, ingredient_name ]
+      existing_ingredient = Ingredient.first( :name => dirty_ingredient[2] )
+      # TODO: whis is @ingredients nil?
+      if(existing_ingredient)
+        @ingredients << existing_ingredient
+      else
+        @ingredients << Ingredient.new.copy_from(dirty_ingredient)
+      end
+    end
     return self
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,9 @@ class Ingredient
   include MongoMapper::Document
 
   key :name,  String, :require => true
+
+  def copy_from ingredient
+    # dirty cocktail ingredients : [ amount, doze, ingredient_name ]
+    @name = ingredient[2]
+  end
 end
