012dab8117ab4c82c9a720df8b038bc58bc865d0
Sylvain Veyrié <sylvain.veyrie@gmail.com>
2010-12-04 14:27:01 +0100

    Add other meta-informations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,11 +2,28 @@ class Cocktail
   include MongoMapper::Document
 
   key :name,  String, :require => true, :index => true
+  key :glass, String
+  key :aka, String
+  key :comment, String
+  key :origin, String
+  key :source, String
+  key :method, String
+  key :rate, Float, :index => true
+  key :garnish, String
+
   many :compositions
 
   def copy_from dirty_cocktail
     # properties
-    @name = dirty_cocktail.name
+    @name = to_utf8 dirty_cocktail.name
+    @glass = to_utf8 dirty_cocktail.glass
+    @method = to_utf8 dirty_cocktail.method
+    @rate = dirty_cocktail.rate
+    @garnish = dirty_cocktail.garnish
+    @aka = to_utf8 dirty_cocktail.infos['AKA']
+    @comment = to_utf8 dirty_cocktail.infos['Comment']
+    @origin = to_utf8 dirty_cocktail.infos['Origin']
+    @source = to_utf8 dirty_cocktail.infos['source']
 
     # ingredients
     copy_ingredients_from dirty_cocktail 
@@ -14,13 +31,17 @@ class Cocktail
   end
 
 private
+  def to_utf8 iso
+    Iconv.conv('utf-8', 'ISO-8859-1', iso)
+  end
+
   def copy_ingredients_from dirty_cocktail
     dirty_cocktail.ingredients.each do |dirty_ingredient|
       # dirty cocktail ingredients : [ amount, doze, ingredient_name ]
       # convert it from yaml's iso to mongo's utf-8
       amount = dirty_ingredient[0]
       doze = dirty_ingredient[1]
-      ingredient_name = Iconv.conv('utf-8', 'ISO-8859-1', dirty_ingredient[2])
+      ingredient_name = to_utf8 dirty_ingredient[2]
 
       existing_ingredient = Ingredient.first( :name => ingredient_name )
 
